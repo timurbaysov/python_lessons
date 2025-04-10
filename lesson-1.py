39cd7d5db324cd9b31dfdb3f84d2250e43f375df
Timur Baysov <timurbaysov2006@gmail.com>
2025-04-10 19:43:34 +0300

    Lesson 1
    Birinshi sabaq qosildi

---
+++ lesson-1.py
@@ -0,0 +1,152 @@
+""" 
+Python
+1 - Lesson
+
+print() function
+
++,-,*,/
+//,**,%
+
+f-string
+"""
+
+# print("Hello World!")
+# print("Assalawma aleykum")
+
+
+# print(2+4*2)
+
+# print(19/5)
+# print(20/5)
+# print(16//4)
+# print(15/4)
+# print(15//4)
+# print(5*6)
+# print(2**4)
+
+
+
+
+
+
+
+# print('To\'gizdin darejesi', 9**2, "ge  ten'")
+# print('3x3=',3*3)
+
+
+
+
+
+
+
+
+# print(15/4)
+
+# print(15 % 4)
+
+# print(16**(1/2))
+
+
+
+
+
+
+# print((7+8)/5)
+# print((7+3)/2*5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
